03f59d69612785b36a6898d41812b4a8ae6bb4f4
Daniyal Awan <76515836+daniyalcr9@users.noreply.github.com>
2022-05-24 22:52:58 +0500

    Add files via upload

---
+++ HashTable/Country.java
@@ -0,0 +1,20 @@
+public class Country {
+    public String name;
+    public String language;
+    public int area;
+    public int population;
+
+    public Country (String name, String language, int area, int population){
+        this.name = name;
+        this.language = language;
+        this.area = area;
+        this.population = population;
+    }
+
+    public String toString(){
+        String str = "";
+        str+="[Country name :" + name + "Country Language : " + language + "Country area : " + area +
+                "Country Population" + population + "]";
+        return str;
+    }
+}
